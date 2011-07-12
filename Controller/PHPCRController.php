90bf13b7b34c3929ff8c2bd8f6961809e74d3a94
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-07-12 08:21:05 +0200

    first attempt at a generic controller to persist into phpcr (others may follow)

---
+++ Controller/PHPCRController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Liip\VieBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\Routing\Exception\ResourceNotFoundException;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use FOS\RestBundle\Controller\Annotations\Prefix;
+use FOS\RestBundle\Controller\Annotations\NamePrefix;
+use FOS\RestBundle\View\View;
+
+use Doctrine\ODM\PHPCR\DocumentManager;
+
+/**
+ * @Prefix("liip/vie/phpcr")
+ * @NamePrefix("liip_vie_phpcr_")
+ */
+class PHPCRController
+{
+    /**
+     * @var FOS\RestBundle\View\View
+     */
+    protected $view;
+
+    /**
+     * @var Symfony\Component\HttpFoundation\Request
+     */
+    protected $request;
+
+    /**
+     * @var Doctrine\ODM\PHPCR\DocumentManager
+     */
+    protected $dm;
+
+    /**
+     * @var array
+     */
+    protected $map;
+
+    public function __construct(Request $request, View $view, DocumentManager $dm, array $map)
+    {
+        $this->request = $request;
+        $this->view = $view;
+        $this->dm = $dm;
+        $this->map = $map;
+    }
+
+    /**
+     * @throws \Symfony\Component\Routing\Exception\ResourceNotFoundException
+     * @param array $data
+     * @return \Doctrine\ODM\PHPCR\DocumentRepository
+     */
+    protected function getRepository($data)
+    {
+        if (empty($this->map[$data['a']])) {
+            throw new ResourceNotFoundException($data['a'].' is not mapped to a class');
+        }
+
+        return $this->dm->getRepository($this->map[$data['a']]);
+    }
+
+    /**
+     * Handle article PUT
+     */
+    public function putDocumentAction($id)
+    {
+        $data = $this->request->request->all();
+
+        $repo = $this->getRepository($data);
+        $id = "/vie/$id";
+        $document = $repo->find($id);
+        if (empty($document)) {
+            throw new ResourceNotFoundException($id.' not found');
+        }
+
+        $document->fromJsonLD($data);
+        $this->dm->flush();
+
+        // return the updated version
+        $this->view->setParameters($document->toJsonLd());
+        $this->view->setFormat('json');
+        return $this->view;
+    }
+}
---
+++ DependencyInjection/LiipVieExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Liip\VieBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class LiipVieExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $processor = new Processor();
+        $configuration = new Configuration();
+        $config = $processor->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('config.yml');
+
+        $container->setParameter($this->getAlias().'.map', $config['map']);
+    }
+}
