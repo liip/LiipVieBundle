ebecb01211763bf4f996ff888bd519981f0abca4
Fabian Vogler <fabian@equivalence.ch>
2011-12-02 15:34:38 +0100

    Use classes for a flexible editor

--- Resources/public/js/init-vie-hallo.js
+++ Resources/public/js/init-vie-hallo.js
@@ -1,196 +1,236 @@
+
 if (!VieBundle) {
     var VieBundle = {};
 }
 
-VieBundle.Hallo = function ($) {
-    var vie;
-    var preventSave = false;
+VieBundle.Model = function(bundle, element) {
 
-    this.init = function () {
-        VieBundle.Hallo.initVIE();
+    this.bundle = bundle;
+    this.element = element;
+    this.vie = this.bundle.vie;
 
-        VieBundle.Hallo.loadTags();
+    // load entities with rdfa
+    this.vie.load({element: element}).from('rdfa').execute();
 
-        VieBundle.Hallo.initMidgradEditable();
-    };
+    var subject = this.vie.service('rdfa').getElementSubject(element);
+    this.entity = this.vie.entities.get(subject);
 
-    this.initVIE = function () {
-        var vie = new VIE();
-        vie.EntityManager.entities.bind('add', function(model) {
-            if (typeof model.id == 'string') {
-                model.url = vie_phpcr_path + model.id.substring(1, model.id.length - 1);
-                model.toJSON = model.toJSONLD;
+    // insert settings pane
+    var id = subject.replace(/[^A-Za-z]/g, '-');
+    this.element.prepend('<div class="hiddenfieldsContainer"><div class="hiddenfieldsToggle"></div><div class="hiddenfields"><div class="hiddenfieldsCloseButton"></div><h2>Article settings</h2><div id="articleTagsWrapper"><form><div class="articleTags"><h3>Article tags</h3><input type="text" id="' + id + '-articleTags" class="tags" value="" /></div><div class="articleSuggestedTags"><h3>Suggested tags</h3><input type="text" id="' + id + '-suggestedTags" class="tags" value="" /></div></form></div></div><div class="hiddenfieldsCloseCorner"></div></div>');
+    this.articleTags = this.element.find('.articleTags input');
+    this.suggestedTags = this.element.find('.articleSuggestedTags input');
+
+    // bind toggle events for settings pane
+    this.element.find('.hiddenfieldsToggle').click(function(event) {
+        var context = $(this).closest('.hiddenfieldsContainer');
+        $('.hiddenfields', context).show();
+        $('.hiddenfieldsToggle', context).hide();
+        $('.hiddenfieldsCloseCorner', context).show();
+        $('.hiddenfieldsCloseButton', context).show();
+    });
+
+    var that = this;
+    this.element.find('.hiddenfieldsCloseCorner, .hiddenfieldsCloseButton').click(function(event) {
+        var context = $(this).closest('.hiddenfieldsContainer');
+        $('.hiddenfields', context).hide();
+        $('.hiddenfieldsToggle', context).show();
+        $('.hiddenfieldsCloseCorner', context).hide();
+        $('.hiddenfieldsCloseButton', context).hide();
+        
+        // save on close
+        that.save();
+    });
+
+    // init tags
+    this.initTags();
+    this.loadTags();
+    
+    // init editor
+    this.initEditable();
+};
+
+VieBundle.Model.prototype.initTags = function () {
+
+    var that = this;
+
+    this.articleTags.tagsInput({
+        width:'auto',
+        height: 'auto',
+        onAddTag: function (tag) {
+
+            var entity = that.entity;
+
+            // convert to reference url
+            if (!entity.isReference(tag)) {
+                tag = 'urn:tag:' + tag;
             }
-        });
 
-        vie.use(new vie.RdfaService);
-
-        vie.namespaces.add('sioc', 'http://rdfs.org/sioc/ns#');
-
-        vie.use(new vie.StanbolService({
-            url: "http://cmf.lo/stanbol",
-            proxyDisabled: true
-        }));
-
-        this.vie = vie;
-    };
-
-    this.initMidgradEditable = function () {
-        var vie = this.vie;
-        $('body').midgardEditable({
-            vie: vie,
-            editorOptions: {
-                'dcterms:title': {
-                    plugins: {
-                        'hallooverlay': {},
-                        'hallotoolbarlinebreak': {}
-                    }
-                },
-                'default': {
-                    plugins: {
-                        'halloimage': { 'searchUrl': 'liip/vie/assets/search/', 'vie': vie },
-                        'hallolink': {},
-                        'halloheadings': {},
-                        'halloformat': {'formattings': {'strikeThrough': false, 'underline': false}},
-                        'hallolists': {'lists': {'ordered': false}},
-                        'hallojustify': {},
-                        'halloreundo': {},
-                        'hallotoolbarlinebreak': { 'breakAfter': ['hallolink'] },
-                        'hallooverlay': {}
-                    },
-                    floating: false,
-                    showAlways: true
-                }
-            },
-            deactivated: vieSaveContent
-        });
-    };
-
-    this.vieSaveContent = function (event, args) {
-        var preventSave = this.preventSave;
-        if (preventSave) {
-            setTimeout(this.vieSaveContent, 5000);
-            return;
+            // add tag to entity
+            entity.attributes['<http://purl.org/dc/elements/1.1/subject>'].vie = that.vie;
+            entity.attributes['<http://purl.org/dc/elements/1.1/subject>'].addOrUpdate({'@subject': tag});
         }
+    });
 
-        // Set the modified properties to the model instance
-        args.instance.save(null, {
-            success: function(savedModel, response) {
-                $(document).trigger("vieSaveSuccess");
-                console.log(savedModel.id + " was saved");
-            },
-            error: function(response) {
-                console.log("Save failed");
-                console.log(response);
+    this.suggestedTags.tagsInput({
+        width:'auto',
+        height: 'auto',
+        interactive: false
+    });
+
+    // add suggested tag on click to tags
+    this.element.find('.articleSuggestedTags .tag span').live('click', function(){
+        var tag = $(this).text();
+        that.articleTags.addTag($.trim(tag));
+        that.suggestedTags.removeTag($.trim(tag));
+    });
+};
+
+VieBundle.Model.prototype.loadTags = function () {
+
+    var that = this;
+
+    // load article tags
+    var tags = this.entity.attributes['<http://purl.org/dc/elements/1.1/subject>'].models;
+    jQuery(tags).each(function () {
+        that.articleTags.addTag(this.id);
+    });
+
+    // load suggested tags
+    var text = $(that.element).text();
+    that.vie.analyze({element: $(that.element)}).using(['stanbol']).execute().success(function(enhancements) {
+        return $(enhancements).each(function(i, e) {
+            if (e.attributes['<rdfschema:label>']) {
+                that.suggestedTags.addTag(e.id);
             }
         });
-    };
+    }).fail(function(xhr) {
+        console.log(xhr);
+    });
+};
 
-    this.setPreventSave = function (value) {
-        this.preventSave = value;
-    };
+VieBundle.Model.prototype.initEditable = function () {
 
-    this.initArticleTags = function (ref) {
-        var vie = this.vie;
-
-        $('#articleTags').tagsInput({
-            width:'auto',
-            height: 'auto',
-            onAddTag: function (tag) {
-                console.log(about);
-                console.log(tag);
-
-                var subject = vie.service('rdfa').getElementSubject(ref);
-                var entity = vie.entities.get(subject);
-
-                // convert to reference url
-                if (!entity.isReference(tag)) {
-                    tag = 'urn:tag:' + tag;
+    this.element.midgardEditable({
+        vie: this.vie,
+        model: this.entity,
+        editorOptions: {
+            'dcterms:title': {
+                plugins: {
+                    'hallooverlay': {},
+                    'hallotoolbarlinebreak': {}
                 }
-
-                entity.attributes['<http://purl.org/dc/elements/1.1/subject>'].vie = vie;
-                entity.attributes['<http://purl.org/dc/elements/1.1/subject>'].addOrUpdate({'@subject': tag});
+            },
+            'default': {
+                plugins: {
+                    'halloimage': { 'searchUrl': 'liip/vie/assets/search/', 'vie': this.vie },
+                    'hallolink': {},
+                    'halloheadings': {},
+                    'halloformat': {'formattings': {'strikeThrough': false, 'underline': false}},
+                    'hallolists': {'lists': {'ordered': false}},
+                    'hallojustify': {},
+                    'halloreundo': {},
+                    'hallotoolbarlinebreak': { 'breakAfter': ['hallolink'] },
+                    'hallooverlay': {}
+                },
+                floating: false,
+                showAlways: true
             }
-        });
-    };
+        },
+        deactivated: $.proxy(this.save, this)
+    });
+};
 
-    this.initSuggestedTags = function () {
-        $('#suggestedTags').tagsInput({
-            width:'auto',
-            height: 'auto',
-            interactive: false
-        });
-    };
+VieBundle.Model.prototype.save = function () {
 
-    this.loadTags = function () {
-        var vie = this.vie;
+    // don't save if blocked by dragged images
+    if (this.bundle.preventSave) {
+        setTimeout(this.save, 5000);
+        return;
+    }
 
-        this.initSuggestedTags();
+    // save the Backbone model
+    this.entity.save(null, {
+        success: function(model, response) {
+            $(document).trigger("vieSaveSuccess");
+            console.log(model.id + " was saved");
+        },
+        error: function(response) {
+            console.log("Save failed");
+            console.log(response);
+        }
+    });
+};
 
-        $('[typeof][about]').each(function() {
-            var that = this;
 
-            VieBundle.Hallo.initArticleTags(that);
+VieBundle.Hallo = function () {
 
-            // load article tags
-            vie.load({element: that}).from('rdfa').execute().done(function(entities) {
-                var tags = entities[0].attributes['<http://purl.org/dc/elements/1.1/subject>'].models;
-                jQuery(tags).each(function (key, value) {
-                    $('#articleTags').addTag(value.id);
-                });
-            });
+    this.preventSave = false;
+    
+    this.initVIE();
 
-            // load suggested tags
-            var text = $(that).text();
-            vie.analyze({element: $(that)}).using(['stanbol']).execute().success(function(enhancements) {
-                return $(enhancements).each(function(i, e) {
-                    if (e.attributes['<rdfschema:label>']) {
-                        $('#suggestedTags').addTag(e.id);
-                    }
-                });
-            }).fail(function(xhr) {
-                console.log(xhr);
-            });
-        });
+    var that = this;
+    $('[typeof][about]').each(function() {
+       
+        var element = $(this);
+        
+        var model = new VieBundle.Model(that, element);
+    });
+};
 
-    };
+VieBundle.Hallo.prototype.initVIE = function () {
+    
+    this.vie = new VIE();
+    this.vie.EntityManager.entities.bind('add', function(model) {
+        if (typeof model.id == 'string') {
+            model.url = vie_phpcr_path + model.id.substring(1, model.id.length - 1);
+            model.toJSON = model.toJSONLD;
+        }
+    });
 
-    return this;
-}(jQuery);
+    this.vie.use(new this.vie.RdfaService());
+
+    this.vie.namespaces.add('sioc', 'http://rdfs.org/sioc/ns#');
+    this.vie.namespaces.add('dc', 'http://purl.org/dc/elements/1.1/');
+
+    this.vie.use(new this.vie.StanbolService({
+        url: "http://cmf.lo/stanbol",
+        proxyDisabled: true
+    }));
+};
+
+VieBundle.Hallo.prototype.setPreventSave = function (value) {
+    this.preventSave = value;
+};
+
 
 jQuery(document).ready(function ($) {
 
-    VieBundle.Hallo.init();
+    var bundle = new VieBundle.Hallo();
 
     $(this).bind('startPreventSave', function() {
-        VieBundle.Hallo.setPreventSave(true);
+        bundle.setPreventSave(true);
     });
 
     $(this).bind('stopPreventSave', function() {
-        VieBundle.Hallo.setPreventSave(false);
+        bundle.setPreventSave(false);
     });
 
-    $(window).resize(function(){
+    $(window).resize(function() {
         if(!$('.inEditMode')[0] == undefined ){
             $('.halloToolbar').css('left', $('.inEditMode')[0].offsetLeft);
         }
     });
 
-    $("#suggestedTags_tagsinput .tag span").live("click", function(){
-        var tag = $(this).text();
-        $('#articleTags').addTag($.trim(tag));
-        $('#suggestedTags').removeTag($.trim(tag));
-    });
-
+    // show edit button on hover
     $('[contenteditable]').not('.inEditMode').hover(function(){
         $(document).trigger('startPreventSave');
         if(!$(this).hasClass('inEditMode')){
             var editButton = $('<div>edit</div>').addClass('editButton');
-            $(this).append(editButton);
+            $(this).before(editButton);
         }
     }, function(){
         $(document).trigger('stopPreventSave');
         $('.editButton').remove();
     });
-
 });
