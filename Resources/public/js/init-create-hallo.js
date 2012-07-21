cae6fd0e2c758afc39bc609085899ddf6cfa7a05
David Buchmann <david.buchmann@liip.ch>
2012-07-21 20:49:15 +0200

    update to latest create.js, adjust create init js to new split up config injection

--- Resources/public/js/init-create-hallo.js
+++ Resources/public/js/init-create-hallo.js
@@ -6,53 +6,61 @@ jQuery(document).ready(function() {
             }
             return vie_phpcr_path;
         },
-        editorOptions: {
-            'dcterms:title': {
-                plugins: {
-                    'hallooverlay': {},
-                    'hallotoolbarlinebreak': {}
-                }
-            },
-            'default': {
-                plugins: {
-                    'halloimage': {
-                        search: function (query, limit, offset, successCallback) {
-                            limit = limit || 8;
-                            offset = offset || 0;
-                            jQuery.ajax({
-                                type: "GET",
-                                url: vie_plugins_image_search,
-                                data: "query="+query+"&offset="+offset+"&limit="+limit,
-                                success: successCallback
-                            });
-                        },
-                        suggestions: function(tags, limit, offset, successCallback) {
-                            limit = limit || 8;
-                            offset = offset || 0;
-                            return jQuery.ajax({
-                                type: "GET",
-                                url: "/app_dev.php/liip/vie/assets/list/",
-                                data: "tags=" + tags + "&offset=" + offset + "&limit=" + limit,
-                                success: successCallback
-                            });
-                        },
-                        'vie': this.vie
-                    },
-                    'hallolink': { 'relatedUrl': vie_plugins_link_related_path },
-                    'halloformat': {'formattings': {'strikeThrough': false, 'underline': false}},
-                    'halloblock': {},
-                    'hallolists': {'lists': {'ordered': false}},
-                    'hallojustify': {},
-                    'hallotoolbarlinebreak': { 'breakAfter': ['hallolink'] },
-                    'hallooverlay': {}
-                },
-                floating: false,
-                fixed: false,
-                parentElement: "body",
-                showAlways: true
-            }
-        },
-        stanbolUrl: 'http://dev.iks-project.eu:8080',
+        stanbolUrl: 'http://dev.iks-project.eu:8081',
         tags: true
     });
+
+    jQuery('body').midgardCreate('configureEditor', 'title', 'halloWidget', {
+        plugins: {
+            'hallooverlay': {},
+            'hallotoolbarlinebreak': {},
+            'halloindicator': {}
+        },
+        floating: false,
+        fixed: true,
+        parentElement: "body",
+        showAlways: true
+    });
+    jQuery('body').midgardCreate('configureEditor', 'default', 'halloWidget', {
+        plugins: {
+            'halloimage': {
+                search: function (query, limit, offset, successCallback) {
+                    limit = limit || 8;
+                    offset = offset || 0;
+                    jQuery.ajax({
+                        type: "GET",
+                        url: vie_plugins_image_search,
+                        data: "query="+query+"&offset="+offset+"&limit="+limit,
+                        success: successCallback
+                    });
+                },
+                suggestions: function(tags, limit, offset, successCallback) {
+                    limit = limit || 8;
+                    offset = offset || 0;
+                    return jQuery.ajax({
+                        type: "GET",
+                        url: "/app_dev.php/liip/vie/assets/list/",
+                        data: "tags=" + tags + "&offset=" + offset + "&limit=" + limit,
+                        success: successCallback
+                    });
+                },
+                'vie': this.vie
+            },
+            'hallolink': { 'relatedUrl': vie_plugins_link_related_path },
+            'halloformat': {'formattings': {'strikeThrough': false, 'underline': false}},
+            'halloblock': {},
+            'hallolists': {'lists': {'ordered': false}},
+            'hallojustify': {},
+            'hallotoolbarlinebreak': { 'breakAfter': ['hallolink'] },
+            'hallooverlay': {},
+            'halloindicator': {}
+        },
+        floating: false,
+        fixed: true,
+        parentElement: "body",
+        showAlways: true
+    });
+
+    jQuery('body').midgardCreate('setEditorForProperty', 'dcterms:title', 'title');
+    jQuery('body').midgardCreate('setEditorForProperty', 'default', 'default');
 });
